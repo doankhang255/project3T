6944c1ea0709fadbd9800e4b8a9f934df8ca2d4f
Doãn Khang <doankhangll255@gmail.com>
2026-04-21 21:53:29 +0700

    da

---
+++ merge_file.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+file1 = "historical_price_2973.parquet"
+file2 = "parquet_all1.parquet"
+output_file = "parquet_all.parquet"
+
+df1 = pd.read_parquet(file1)
+df2 = pd.read_parquet(file2)
+
+df_all = pd.concat([df1, df2], axis=0, ignore_index=True)
+
+df_all.to_parquet(output_file, index=False)
+
+print("Rows file1:", len(df1))
+print("Rows file2:", len(df2))
+print("Rows all:", len(df_all))
